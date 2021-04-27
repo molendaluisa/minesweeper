92b10790289e73f21aead2b8aef7212fadd8ed92
Luisa Molenda <77063747+molendaluisa@users.noreply.github.com>
2021-04-27 14:03:55 +0200

    populating board with mines done

--- minesweeper.js
+++ minesweeper.js
@@ -1,14 +1,61 @@
 //Game logic
 
+const TILE_STATUSES = {
+  HIDDEN: 'hidden',
+  MINE: 'mine',
+  NUMBER: 'number',
+  MARKED: 'marked',
+}
+
+
 export function createBoard(boardSize, numberOfMines) {
   const board = []
+  const minePositions = getMinePositions(boardSize, numberOfMines)
   for (let x = 0; x < boardSize; x++) {
     const row = []
     for (let y = 0; y < boardSize; y++) {
-      const tile = { x, y }
+      const element = document.createElement('div')
+      element.dataset.status = TILE_STATUSES.HIDDEN
+      const tile = {
+        element,
+        x,
+        y,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return element.dataset.status
+        },
+        set status(value) {
+          this.element.dataset.status = value
+        }
+      }
       row.push(tile)
     }
     board.push(row)
   }
   return board
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = []
+
+  while (positions.lenght < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize)
+    }
+
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.pus(position)
+    }
+  }
+
+  return positions
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.rando() * size)
 }
--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-import { createBoard } from './minesweeper.js'
+//Game UI
 
-console.log(createBoard(2, 2))
+import { createBoard } from './minesweeper.js';
+
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 2
+
+//populating board
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector('.board');
+const minesLeftText = document.querySelector('[data-mine-count]')
+boardElement.style.setProperty("--size", BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element)
+  })
+})
